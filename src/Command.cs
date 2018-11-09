471bccfd749a3d14b30988e2fd045314c6a65606
natiiix <natiwastaken@gmail.com>
2018-11-09 23:56:16 +0100

    Prepare command parser logic for addition of actual commands

--- src/Command.cs
+++ src/Command.cs
@@ -4,6 +4,19 @@ namespace Bamboo
     {
         public static Command Parse(string str)
         {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return null;
+            }
+
+            string lower = str.ToLower();
+
+            switch (lower)
+            {
+                default:
+                    break;
+            }
+
             return null;
         }
     }
